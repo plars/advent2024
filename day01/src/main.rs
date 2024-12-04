bd862e6f7b209753635b98942b81b705c424c822
Paul Larson <pwlars@gmail.com>
2024-12-03 22:07:22 -0600

    day01b

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
-fn part_a(data: String) -> u32 {
+fn part_a(data: &String) -> u32 {
     let mut left: Vec<u32> = Vec::new();
     let mut right: Vec<u32> = Vec::new();
 
@@ -20,8 +21,25 @@ fn part_a(data: String) -> u32 {
     total
 }
 
+fn part_b(data: &String) -> u32 {
+    let mut left: Vec<u32> = Vec::new();
+    let mut right: HashMap<u32, u32> = HashMap::new();
+
+    for line in data.lines() {
+        let (l, r) = line.split_once("   ").unwrap();
+        left.push(l.parse::<u32>().unwrap());
+        *right.entry(r.parse::<u32>().unwrap()).or_insert(0) += 1;
+    }
+
+    let mut total: u32 = 0;
+    for i in left.iter() {
+        total += i * right.get(i).unwrap_or(&0);
+    }
+    total
+}
+
 fn main() {
     let data = read_to_string("input.txt").unwrap();
-    //let data = "1 2\n3 4\n5 6\n7 8".to_string();
-    println!("{}", part_a(data));
+    println!("{}", part_a(&data));
+    println!("{}", part_b(&data));
 }

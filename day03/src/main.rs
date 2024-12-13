0f7fec13df173bb602e12c6a716c5055c4a519bf
Paul Larson <pwlars@gmail.com>
2024-12-13 14:16:20 -0600

    We can consume these since we won't use them again

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -5,7 +5,7 @@ fn part_a(data: String) -> i32 {
     let matcher = Regex::new(r"mul\((?<num1>[0-9]+),(?<num2>[0-9]+)\)").unwrap();
     let products: Vec<i32> = matcher
         .captures_iter(data.as_str())
-        .map(|caps| &caps["num1"].parse::<i32>().unwrap() * &caps["num2"].parse::<i32>().unwrap())
+        .map(|caps| caps["num1"].parse::<i32>().unwrap() * caps["num2"].parse::<i32>().unwrap())
         .collect();
     products.iter().sum()
 }

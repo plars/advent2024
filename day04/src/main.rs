e3603b594913afdc5eef3cfcea1f888951ab7e99
Paul Larson <pwlars@gmail.com>
2024-12-23 15:47:49 -0600

    Really bad first pass at day04a

---
+++ day04/src/main.rs
@@ -0,0 +1,84 @@
+use std;
+
+fn part_a(data: &String) -> i32 {
+    let mut count = 0;
+    let wordsearch = make_wordsearch(&data);
+    for (y, line) in wordsearch.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            count += match c {
+                'X' => check_pattern_from_location(&wordsearch, "MAS", x, y),
+                'S' => check_pattern_from_location(&wordsearch, "AMX", x, y),
+                _ => 0,
+            }
+        }
+    }
+
+    count
+}
+
+fn make_wordsearch(data: &String) -> Vec<Vec<char>> {
+    let mut wordsearch: Vec<Vec<char>> = Vec::new();
+    for line in data.lines() {
+        wordsearch.push(line.chars().collect());
+    }
+    wordsearch
+}
+
+fn check_pattern_from_location(
+    wordsearch: &Vec<Vec<char>>,
+    pattern: &str,
+    x: usize,
+    y: usize,
+) -> i32 {
+    // starting from the specified x,y location, check how many times the remaining characters are found in any direction
+    let mut count: i32 = 0;
+    let max_y = wordsearch.len() - 1;
+    let max_x = wordsearch[0].len() - 1;
+
+    // check east
+    if x + pattern.len() <= max_x {
+        if wordsearch[y][x + 1..x + pattern.len() + 1]
+            .iter()
+            .map(|&c| c)
+            .eq(pattern.chars())
+        {
+            count += 1;
+        }
+    }
+
+    //check south
+    if y + pattern.len() <= max_y {
+        if (1..pattern.len() + 1)
+            .map(|i| wordsearch[y + i][x])
+            .eq(pattern.chars())
+        {
+            count += 1;
+        }
+    }
+
+    //check southeast
+    if x + pattern.len() <= max_x && y + pattern.len() <= max_y {
+        if (1..pattern.len() + 1)
+            .map(|i| wordsearch[y + i][x + i])
+            .eq(pattern.chars())
+        {
+            count += 1;
+        }
+    }
+
+    //check southwest
+    if x as i32 - pattern.len() as i32 >= 0 && y + pattern.len() <= max_y {
+        if (1..pattern.len() + 1)
+            .map(|i| wordsearch[y + i][x - i])
+            .eq(pattern.chars())
+        {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn main() {
+    let data = std::fs::read_to_string("input.txt").unwrap();
+    println!("{}", part_a(&data));
+}
